5148b60936cd5959ad3e9a9d528c2fc190ae1a74
BuildTools <llin11@stuy.edu>
2020-11-03 14:24:47 -0500

    Add function headers to song.h

---
+++ song.h
@@ -0,0 +1,21 @@
+#ifndef SONG_H
+#define SONG_H
+
+struct song{
+  char name[128];
+  char artist[128];
+  struct song *next;
+};
+
+struct song *make_song(char *new_name, char *new_artist);
+struct song *insert_front(struct song *head, struct song *new_song);
+struct song *insert_order(); // ASK DW
+struct song *insert_alpha(struct song *head, struct song *new_song);
+void print_list(struct song *head);
+struct song *find_song(struct song *head, char *artist, char *song);
+struct song *first_song(struct song *head, char *artist);
+struct song *random_song(struct song *head);
+struct song *remove_song(struct song *head, struct song *song);
+struct song *free_songs(struct song *head);
+
+#endif
