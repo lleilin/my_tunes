80b5186ee845940e03eda1b3f4437a9fe15b0146
BuildTools <llin11@stuy.edu>
2020-11-05 14:25:26 -0500

    Update main and change naming in song.c

--- song.c
+++ song.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include "song.h"
-struct song *make_song(char *nn, char *na) {
+
+struct song *make_song(char *new_name, char *new_artist) {
     struct song *ns = malloc(sizeof(struct song));
     strncpy(ns->name,nn,sizeof(ns->name)-1);
     strncpy(ns->artist,na,sizeof(ns->artist)-1);
     ns->next = NULL;
-    
+
     return ns;
 }
 
