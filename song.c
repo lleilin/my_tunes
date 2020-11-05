0c07ebbdd6d57faec26331520a644b8cbc04398a
jku20 <jku-ben20@stuy.edu>
2020-11-05 11:11:05 -0500

    implemented find_song and insert_front

--- song.c
+++ song.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+#include "song.h"
 struct song *make_song(char *nn, char *na) {
     struct song *ns = malloc(sizeof(struct song));
     strncpy(ns->name,nn,sizeof(ns->name)-1);
@@ -5,4 +7,21 @@ struct song *make_song(char *nn, char *na) {
     ns->next = NULL;
     
     return ns;
+}
+
+struct song *insert_front(struct song *head, struct song *new_song) {
+    new_song->new_song = head;
+    return new_song;
+}
+
+struct song *find_song(struct song *head, char *artist, char *song) {
+    struct song *cur = head;
+    while(head) {
+        if(!strcmp(artist,cur->artist) && !strcmp(song,cur->name)) {
+            return cur;
+        }
+        cur = cur->next;
+    }
+
+    return NULL;
 }
