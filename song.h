b0071b7ca98fe163e4a92b9eba643f35972d4444
BuildTools <llin11@stuy.edu>
2020-11-07 09:39:24 -0500

    Remove insert_alpha

--- song.h
+++ song.h
@@ -10,7 +10,6 @@ struct song{
 struct song *make_song(char *new_name, char *new_artist);
 struct song *insert_front(struct song *head, struct song *new_song);
 struct song *insert_order(struct song *head, struct song *new_song); 
-struct song *insert_alpha(struct song *head, struct song *new_song);
 void print_list(struct song *head);
 struct song *find_song(struct song *head, char *artist, char *song);
 struct song *first_song(struct song *head, char *artist);
