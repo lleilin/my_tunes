3e07f76d4f0c681941d1a8532b8d2e3bb104162a
Yulin <zhengyulin@gmail.com>
2020-11-07 11:04:46 -0500

    fixed main

--- main.c
+++ main.c
@@ -5,7 +5,13 @@
 #include "song.h"
 int main() {
 	struct song *songs = NULL;
-	songs.insert_front("Song 1", "Lei");
-	songs.insert_front("Song 2", "Jeremy");
+	songs.insert_front(songs, "Song 1", "Lei");
+	songs.insert_front(songs, "Song 2", "Jeremy");
+	songs.insert_front(songs, "Song 3", "Yulin");
+	songs.insert_front(songs, "Song 4", "Lei");
+	songs.insert_front(songs, "Song 5", "Jeremy");
+	songs.insert_front(songs, "Song 6", "Yulin");
+	print_all(songs);
+	
 	return 0;
 }
